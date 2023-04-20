179000d20a944528eefb706eeeadb9ae2bed18eb
Anastasia Hohlova <stasy002@mail.ru>
2023-04-20 22:10:44 +0400

    second commit

--- WebProject/server/controllers/DishController.js
+++ WebProject/server/controllers/DishController.js
@@ -1,6 +1,25 @@
-class DishController {
-    async create(req,res){
+const uuid = require('uuid')
+const path = require('path')
+const {Dish} = require('../models/models')
+const ApiError = require('../error/ApiError')
 
+class DishController {
+    async create(req,res, next){
+        try{
+            
+            const {name, typeID, description, weight, calories, cost, dish_info} = req.body
+            const {img} = req.files
+            let fileName = uuid.v4()+'.jpg'
+            img.mv(path.resolve(__dirname,'..','static', fileName))
+            const dish = await Dish.create({name, typeID, description, weight, calories, cost, dish_info, img: fileName})
+            return res.json(dish)
+        }
+        catch(e)
+        {
+            next(ApiError.badRequest(e.message))
+        }
+
+        
     }
 
     async getAll(req,res){
