0508300d5f53de410ca4546fac5d7ef918280cdd
Zhuravel17 <kat.20030304@yandex.ru>
2023-05-02 17:34:00 +0400

    basket, error

--- WebProject/client/src/components/modals/CreateDish.js
+++ WebProject/client/src/components/modals/CreateDish.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import { Button, Form, Modal } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Button, Dropdown, Form, Modal } from 'react-bootstrap';
+import { Context } from '../..';
 
 const CreateDish = ({show, onHide}) => {
+    const { dish } = useContext(Context)
+    const [info, setInfo] = useState([])
     return (
         <Modal
             show={show}
@@ -11,14 +14,47 @@ const CreateDish = ({show, onHide}) => {
         >
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
-                    Добавить тип
+                    Добавить блюдо
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <Form>
-                    <Form.Control
-                        placeholder='Введите название типа'
+                    <Dropdown>
+                        <Dropdown.Toggle>Выберите тип блюда</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {dish.types.map(type =>
+                                <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+                                )}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Form.Control 
+                    className='mt-3'
+                    placeholder='Введите название блюда'
                     />
+                    <Form.Control 
+                    className='mt-3'
+                    placeholder='Введите описание блюда'
+                    />
+                    <Form.Control 
+                    className='mt-3'
+                    placeholder='Введите вес блюда'
+                    type='number'
+                    />
+                    <Form.Control 
+                    className='mt-3'
+                    placeholder='Введите калорийность блюда'
+                    type='number'
+                    />
+                    <Form.Control 
+                    className='mt-3'
+                    placeholder='Введите цену блюда'
+                    type='number'
+                    />
+                    <Form.Control 
+                    className='mt-3'
+                    type='file'
+                    />
+                    <hr/>
                 </Form>
             </Modal.Body>
             <Modal.Footer>
