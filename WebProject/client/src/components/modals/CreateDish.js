27406dad37b76f865554a9e0cf0b1a69e9f12dd7
Zhuravel17 <kat.20030304@yandex.ru>
2023-05-28 15:12:16 +0400

    add dish

--- WebProject/client/src/components/modals/CreateDish.js
+++ WebProject/client/src/components/modals/CreateDish.js
@@ -1,10 +1,48 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Button, Dropdown, Form, Modal } from 'react-bootstrap';
 import { Context } from '../..';
+import { observer } from 'mobx-react-lite';
+import { createDish, fetchDishes, fetchType } from '../../http/dishAPI';
 
-const CreateDish = ({show, onHide}) => {
+const CreateDish = observer(({ show, onHide }) => {
     const { dish } = useContext(Context)
+    const [type, setType] = useState('')
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
+    const [weight, setWeight] = useState(0)
+    const [ccal, setCcal] = useState(0)
+    const [cost, setCost] = useState(0)
+    const [file, setFile] = useState(null)
     const [info, setInfo] = useState([])
+
+    useEffect(()=>{
+        fetchType().then(data=>dish.setTypes(data))
+        fetchDishes().then(data=>dish.setDishes(data.rows))
+    }, [])
+
+    const selectFile = e => {
+        setFile(e.target.files[0])
+    }
+
+    const addInfo = () => {
+        setInfo([...info, {title: '', description: '', number: Date.now()}])
+    }
+    const removeInfo = (number) => {
+        setInfo(info.filter(i => i.number !== number))
+    }
+
+    const addDish =()=>{
+        const formData = new FormData()
+        formData.append('name', name)
+        formData.append('description', description)
+        formData.append('weight', `${weight}`)
+        formData.append('calories', `${ccal}`)
+        formData.append('cost', `${cost}`)
+        formData.append('photo', file)
+        formData.append('typeId', dish.selectedType.id)
+        formData.append('info', JSON.stringify(info))
+        createDish(formData).then(data => onHide())
+    }
     return (
         <Modal
             show={show}
@@ -20,49 +58,60 @@ const CreateDish = ({show, onHide}) => {
             <Modal.Body>
                 <Form>
                     <Dropdown>
-                        <Dropdown.Toggle>Выберите тип блюда</Dropdown.Toggle>
+                        <Dropdown.Toggle>{ dish.selectedType.name ||"Выберите тип блюда"}</Dropdown.Toggle>
                         <Dropdown.Menu>
                             {dish.types.map(type =>
-                                <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
-                                )}
+                                <Dropdown.Item onClick={()=>dish.setSelectedType(type)} key={type.id}>{type.name}</Dropdown.Item>
+                            )}
                         </Dropdown.Menu>
                     </Dropdown>
-                    <Form.Control 
-                    className='mt-3'
-                    placeholder='Введите название блюда'
+                    <Form.Control
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        className='mt-3'
+                        placeholder='Введите название блюда'
                     />
-                    <Form.Control 
-                    className='mt-3'
-                    placeholder='Введите описание блюда'
+                    <Form.Control
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                        className='mt-3'
+                        placeholder='Введите описание блюда'
                     />
-                    <Form.Control 
-                    className='mt-3'
-                    placeholder='Введите вес блюда'
-                    type='number'
+                    <Form.Control
+                        value={weight}
+                        onChange={e => setWeight(Number(e.target.value))}
+                        className='mt-3'
+                        placeholder='Введите вес блюда'
+                        type='number'
                     />
-                    <Form.Control 
-                    className='mt-3'
-                    placeholder='Введите калорийность блюда'
-                    type='number'
+                    <Form.Control
+                        value={ccal}
+                        onChange={e => setCcal(Number(e.target.value))}
+                        className='mt-3'
+                        placeholder='Введите калорийность блюда'
+                        type='number'
                     />
-                    <Form.Control 
-                    className='mt-3'
-                    placeholder='Введите цену блюда'
-                    type='number'
+                    <Form.Control
+                        value={cost}
+                        onChange={e => setCost(Number(e.target.value))}
+                        className='mt-3'
+                        placeholder='Введите цену блюда'
+                        type='number'
                     />
-                    <Form.Control 
-                    className='mt-3'
-                    type='file'
+                    <Form.Control
+                        className='mt-3'
+                        type='file'
+                        onChange={selectFile}
                     />
-                    <hr/>
+                    <hr />
                 </Form>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant='outline-danger' onClick={onHide}>Закрыть</Button>
-                <Button variant='outline-success' onClick={onHide}>Добавить</Button>
+                <Button variant='outline-success' onClick={addDish}>Добавить</Button>
             </Modal.Footer>
         </Modal>
     );
-};
+});
 
 export default CreateDish;
