0f5e8b36a5294a4c7ecfcada05eca2cef30d0738
Zhuravel17 <kat.20030304@yandex.ru>
2023-06-01 22:13:51 +0400

    basket

---
+++ WebProject/client/src/store/BasketStore.js
@@ -0,0 +1,28 @@
+import {makeAutoObservable} from "mobx";
+
+export default class BasketStore {
+    constructor() {
+        this._isAuth = false
+        this._user = {}
+        this._role = ""
+
+        makeAutoObservable(this)
+    }
+
+    setIsAuth(bool) {
+        this._isAuth = bool
+    }
+    setUser(user) {
+        this._user = user
+    }
+    setRole(string) {
+        this._role = string
+    }
+
+    get isAuth() {
+        return this._isAuth
+    }
+    get user() {
+        return this._user
+    }
+}
